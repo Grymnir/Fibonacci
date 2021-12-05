2430697fa0941e1b5bf4c50e3ad6108d4c1c69ea
Grymnir <magnus.carl.ekman@gmail.com>
2021-12-05 03:35:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FibonacciUser
+{
+    class Program
+    {
+        public static int Fibonacci(int numbers)
+        {
+            int a = 0;
+            int b = 1;
+
+
+            for (int i = 0; i < numbers; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with Fibonacci numbers!!");
+            Console.WriteLine("How many numbers do you want?");
+            int fibonacci = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < fibonacci; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
